3684f1ece73f9a99d20e8d77111f816dae0cb364
verah <verahnyagaka101@gmail.com>
2022-06-07 08:20:02 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 2,num2 = 31;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 34, num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(11, 2, 13, 4, 20);
